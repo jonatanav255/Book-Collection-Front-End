0a6c739734698a971645f36264cbac0e0a5a640c
Jonatan <jonatanav255@hotmail.com>
2026-02-22 19:02:02 -0600

    deployments

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   images: {
     remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.books.jonathan-dev.com',
+        pathname: '/api/books/**',
+      },
       {
         protocol: 'http',
         hostname: 'localhost',
